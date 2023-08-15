14593268954b640b38fcefa00b8b53b205af4543
Faiem-qy <f.quanyoung@gmail.com>
2023-08-14 23:45:04 -0400

    made changes

--- iss.js
+++ iss.js
@@ -7,12 +7,33 @@
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
 
-const request = require('requrest');
+const request = require('request');
 
 
 const fetchMyIP = function(callback) {
   // use request to fetch IP address from JSON API
-const url =  `https://api.ipify.org?format=jsonp&callback=getip`
+  const url = `https://api.ipify.org?format=jsonp&callback=getip`;
+
+
+  request(url, (err, response, body) => {
+    if (err) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(`Request failed with status code ${response.statusCode}`, null);
+      return;
+    }
+    try {
+      const data = JSON.parse(body);
+      const ip = data.ip;
+      callback(null, ip);
+    } catch (parseError) {
+      callback(parseError, null);
+    }
+
+  });
 };
 
 module.exports = { fetchMyIP };
