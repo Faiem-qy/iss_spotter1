cd3c9e4400142825edccf68253ac8c4627236ca9
Faiem-qy <f.quanyoung@gmail.com>
2023-08-16 23:32:11 -0400

    implement fetchflyovertimes and fetchcoordsbyip

--- iss_promised.js
+++ iss_promised.js
@@ -7,4 +7,19 @@ const fetchMyIP = function() {
 
 
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`http://ipwho.is/${ip}`);
+};
+
+
+const fetchISSFlyOverTimes = function(body) {
+  const { latitude, longitude } = JSON.parse(body);
+  const url = `https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`;
+  return request(url);
+};
+
+
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes,nextISSTimesForMyLocation };
