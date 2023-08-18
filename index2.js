7baf03aca220cf1bfff7364dc678fe769353be00
Faiem-qy <f.quanyoung@gmail.com>
2023-08-17 21:04:04 -0400

    ISS Spotter with promises

--- index2.js
+++ index2.js
@@ -1,6 +1,20 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss_promised');
+const { nextISSTimesForMyLocation} = require('./iss_promised');
 
-fetchMyIP()
-  .then(fetchCoordsByIP)
-  .then(fetchISSFlyOverTimes)
-  .then(body => console.log(body));
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {  //return the promise that is returned by the request
+    printPassTimes(passTimes);  //the request, when called, returns a promise, and we want our function to return this same promise
+  })
+  .catch((error) => {
+    console.log("It didn't work:", error.message);
+  });
+
+  // passtimes not defined error
--- iss_promised.js
+++ iss_promised.js
@@ -1,10 +1,20 @@
-const request = require("request-promise-native");
+//Async code will not be returned immediately
+//The asynchronous method returns a promise to supply the value at some point in the future.
+
+//The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
+//Essentially, a promise is a returned object to which you attach callbacks, instead of passing callbacks into a function
+//.then(handleFulfilledA, handleRejectedA)
+
+//When either of these options occur(fullfilled or rejected)), the associated handlers queued up by a promise's then method are called
+//The .then() method takes up to two arguments; the first argument is a callback function for the fulfilled case of the promise, and the second argument is a callback function for the rejected case. Each .then() returns a newly generated promise object
+//.then(handleFulfilledA, handleRejectedA)
+
+const request = require("request-promise-native");  // when used this returns a promise?
 
 
 const fetchMyIP = function() {
-  return request(`https://api.ipify.org?format=json`);
+  return request(`https://api.ipify.org?format=json`);  //fetches our IP address from an API and returns the data as a promise
 };
-
 
 
 const fetchCoordsByIP = function(body) {
@@ -20,6 +30,14 @@ const fetchISSFlyOverTimes = function(body) {
 };
 
 
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)  //gives the ip from fetchMyIp to this function
+    .then(fetchISSFlyOverTimes)  //gives the coords from fetchCoords to this func
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
 
-
-module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes,nextISSTimesForMyLocation };
+module.exports = { nextISSTimesForMyLocation };
